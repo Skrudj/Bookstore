9c2906a56e9e035e5cc33370fdd77502b90320e0
Skrudj <47216675+Skrudj@users.noreply.github.com>
2024-04-02 17:58:30 +0300

    implement DTO validation and GlobalExceptionHandler
    * implement DTO validation and GlobalExceptionHandler

--- src/main/java/com/example/bookstore/dto/request/BookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/request/BookRequestDto.java
@@ -1,14 +1,21 @@
 package com.example.bookstore.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookRequestDto {
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
