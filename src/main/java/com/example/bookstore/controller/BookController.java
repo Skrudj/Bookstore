703284cc1edd93127fee9f33bb0aff63ab700717
Skrudj <ssinilov@gmail.com>
2024-04-04 13:44:20 +0300

    add swagger to project

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -7,6 +7,8 @@ import com.example.bookstore.exeptions.EntityNotFoundException;
 import com.example.bookstore.mappper.BookMapper;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +27,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
+@Tag(name = "Book Entity Endpoints")
 public class BookController {
     private final BookService bookService;
     private final BookMapper bookMapper;
 
     @GetMapping
+    @Operation(
+            summary = "Get all books",
+            description = "Endpoint to get all books. Supports pagination"
+    )
     public List<BookResponseDto> findAll(Pageable pageable) {
         return bookService
                 .findAll(pageable)
@@ -39,6 +46,7 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get book by Id")
     public BookResponseDto findById(@PathVariable Long id) {
         Book book = bookService.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Book by id " + id + " not found")
@@ -48,6 +56,10 @@ public class BookController {
     }
 
     @GetMapping("/search")
+    @Operation(
+            summary = "Search book",
+            description = "Endpoint for searching books by severe parameters"
+    )
     public List<BookResponseDto> search(@RequestBody BookSearchParameters params) {
         return bookService
                 .search(params)
@@ -58,18 +70,21 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create book")
     public BookResponseDto save(@RequestBody @Valid BookRequestDto request) {
         Book book = bookMapper.toModel(request);
         return bookMapper.toDto(bookService.add(book));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update book")
     public void update(@PathVariable Long id, @RequestBody BookRequestDto request) {
         Book book = bookMapper.toModel(request);
         bookService.update(id, book);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book")
     public void delete(@PathVariable Long id) {
         bookService.delete(id);
     }
--- src/main/java/com/example/bookstore/dto/request/BookSearchParameters.java
+++ src/main/java/com/example/bookstore/dto/request/BookSearchParameters.java
@@ -1,7 +1,6 @@
 package com.example.bookstore.dto.request;
 
 import java.math.BigDecimal;
-import java.util.List;
 import lombok.Data;
 
 @Data
@@ -10,7 +9,6 @@ public class BookSearchParameters {
     private String isbn;
     private String author;
     private String description;
-    private List<String> tags;
     private BigDecimal lowerPrice;
     private BigDecimal upperPrice;
 }
